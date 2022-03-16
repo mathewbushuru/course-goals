2a1228bc626453df64c9e2e5f4939df3bfc31b8f
Mathew Bushuru <bushuruwafulamathew@gmail.com>
2022-03-16 16:58:11 -0700

    course title, course goal input, course add button

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ padding: 50 }}>
+      <Text>COURSE GOALS!</Text>
+
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder="Enter course goal..."
+          style={{
+            width: "80%",
+            borderBottomColor: "black",
+            borderBottomWidth: 1,
+            padding: 10,
+          }}
+        />
+        <Button title="ADD" />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
